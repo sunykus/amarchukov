5bbfa86dc281864b48397a2403e21a5bcbd50fe1
sunykus <sunykus@gmail.com>
2017-10-26 22:33:00 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j;
-
+/**
+* Calculate класс для вычисления арифметических оераций
+* @author amarchukov
+* @since 26.10.2017
+* @version 1
+*/
 public class Calculate {
+/**
+* запускаем метод, выводящий в консоль "Hello World"
+* @param args принимает символы
+* @return метод имеет тип void, т.е. ничего не возвращает
+*/
 
 public static void main(String[] args) {
         System.out.println ("Hello World");
