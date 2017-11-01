68c33b9537f72d70e8a049df2831ae566e84d55b
sunykus <sunykus@gmail.com>
2017-11-01 11:55:33 +0300

    1.4. JUnit [#4122]

---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,30 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Aleksandr Marchukov (mailto:sunykus@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+public class CalculateTest {
+/**
+* Test echo.
+* @Test
+*/
+
+public void whenTakeNameThenTreeEchoPlusName() {
+    String input = "Aleksandr Marchukov";
+    String expect = "Echo, echo, echo : Aleksandr Marchukov"; 
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+}
+ 
+}
---
+++ chapter_001/src/test/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Aleksandr Marchukov (mailto:sunykus@yandex.ru)
+* @version $Id$
+* @since 26.10.2017
+*/
+package ru.job4j;
