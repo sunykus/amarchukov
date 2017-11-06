36b7865bd9ad0b5cf9c3a912e9306bd35124a4db
sunykus <sunykus@gmail.com>
2017-11-06 21:48:01 +0300

    1.4. JUnit [[#4122]#29711]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,27 +1,33 @@
 package ru.job4j;
 /**
-* Calculate класс для вычисления арифметических оераций
-* @author amarchukov
-* @since 26.10.2017
-* @version 1
-*/
+ * Calculate class
+ * @author amarchukov
+ * @since 26.10.2017
+ * @version 1
+ */
 public class Calculate {
-/**
-* запускаем метод, выводящий в консоль "Hello World"
-* @param args принимает символы
-* @return метод имеет тип void, т.е. ничего не возвращает
-*/
+    /**
+     * Р’С‹РІРѕРґРёРј РІ РєРѕРЅСЃРѕР»СЊ "Hello World"
+     * @param args 
+     * @return void
+     */
 
-public static void main(String[] args) {
-        System.out.println ("Hello World");
-		}
-		
-/**
-* Method echo.
-* @param name Your name.
-* @return Echo plus your name.
-*/
-public String echo(String name) {
-    return "Eho, echo, echo : " + name;
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
+
+
+
 }
-}
+
+
+
--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -7,24 +7,23 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Aleksandr Marchukov (mailto:sunykus@yandex.ru)
-* @version $Id$
-* @since 0.1
-*/
+ * Test.
+ *
+ * @author Aleksandr Marchukov (mailto:sunykus@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 public class CalculateTest {
-/**
-* Test echo.
-* @Test
-*/
+    /**
+     * Test echo.
+     * Test
+     */
 
-public void whenTakeNameThenTreeEchoPlusName() {
-    String input = "Aleksandr Marchukov";
-    String expect = "Echo, echo, echo : Aleksandr Marchukov"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-}
- 
+    public void whenTakeNameThenTreeEchoPlusName() {
+        String input = "Aleksandr Marchukov";
+        String expect = "Echo, echo, echo : Aleksandr Marchukov";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
 }
