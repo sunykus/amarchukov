c34aae3cfb129e82c1c674cef27b0bd33eaca927
sunykus <sunykus@gmail.com>
2017-11-19 14:43:57 +0300

    4.1. Подсчет суммы чётных чисел в диапазоне [#192]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Counter {
+    int y;
+    public int add(int start, int finish) {
+        for (int x = start; x <= finish; x++) {
+            if (x % 2 == 0) {
+                y += x;
+            }
+        }
+        return y;
+    }
+}
